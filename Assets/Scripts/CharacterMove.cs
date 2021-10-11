f7e87529daf5ce2a9ebe70cfa0c3da60b63a848d
ysh4267 <ysh4267@gmail.com>
2021-10-11 10:34:02 +0900

    CharacterMove Update

---
+++ Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] Animator Anim; //모션 애니메이션
+    [SerializeField] float moveSpeed = 25f; //이동속도
+
+    void FixedUpdate()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            //움직일때
+            Anim.SetBool("Walk", true);
+
+            rb.velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * moveSpeed; //조이스틱의 x, z벡터로 캐릭터를 이동시킴 (가시성을 위해 z의 y표기)
+            rb.rotation = Quaternion.LookRotation(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))); //캐릭터의 앞을 조이스틱의 x, z각도만큼 돌림
+        }
+        else {
+            Anim.SetBool("Walk", false);
+        }
+    }
+}
